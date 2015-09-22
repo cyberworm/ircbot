00ced00f0a089366cbd9fe0f14e38b9daa2603fd
Alexander Senger <alexander.senger@gmx.net>
2015-09-22 23:22:59 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ircbot',
+      version='0.1',
+      description='A small IRC bot framework for Python',
+      author='Cyberworm',
+      author_email='cybaworm@gmail.com',
+      url='https://github.com/cyberworm/ircbot',
+      packages=['ircbot'],
+      )
